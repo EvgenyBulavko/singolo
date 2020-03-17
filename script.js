e71ee15436076cd30a10dde44004ba2fcf56f4c1
EvgenyBulavko <z-bulavko@mail.ru>
2020-03-17 23:03:33 +0300

    feat: add in script potfolio

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 
 const menu = document.querySelector('.menu')
-//const portfolioGallery = document.getElementById('portfolio-gallery');
+const image = document.getElementById('photo_container');
+const menuProfile = document.getElementById('menu_point');
+let shet = 1;
+
+
 //const portfolioImages = portfolioGallery.querySelectorAll('.portfolio-img');
 //const portfolioTags = document.getElementById('portfolio-tags');
 
@@ -11,4 +15,19 @@ menu.addEventListener('click', (event) => {
   menu.querySelectorAll('.nav-item').forEach((item) => item.classList.remove('red'));
   menuElement.classList.add('red');
 })
+// PORTFOLIO
+image.addEventListener('click', (event) => {
+  image.querySelectorAll('img').forEach(el => el.classList.remove('active_img'));
+  event.target.classList.add('active_img');
+});
+menuProfile.addEventListener('click', (event) => {
+  menuProfile.querySelectorAll('a').forEach(el => el.classList.remove('menu_active'));
+  event.target.classList.add('menu_active');
+  shet++
+  for (let i = 1; i < 13; i++) {
+    document.getElementById(`myImage${i}`).src = `./assets/images/Portfol/${shet}.png`;
+    if (shet === 12) { shet = 0; }
+    shet++;
+  }
 
+});
