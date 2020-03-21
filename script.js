3bf80cdd3708583a2cbb08f24061540e925460ca
EvgenyBulavko <z-bulavko@mail.ru>
2020-03-21 13:39:15 +0300

    feat: add slider

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 
 const menu = document.querySelector('.menu')
 const image = document.getElementById('photo_container');
+
+const scrollLeft1 = document.getElementById('scroll_left1');
+const scrollLeft = document.getElementById('scroll_left');
+
+const scrollRight1 = document.getElementById('scroll_right1');
+const scrollRight = document.getElementById('scroll_right');
 const menuProfile = document.getElementById('menu_point');
 let shet = 1;
 
-
-//const portfolioImages = portfolioGallery.querySelectorAll('.portfolio-img');
-//const portfolioTags = document.getElementById('portfolio-tags');
 
 // HEADER
 menu.addEventListener('click', (event) => {
@@ -15,6 +18,23 @@ menu.addEventListener('click', (event) => {
   menu.querySelectorAll('.nav-item').forEach((item) => item.classList.remove('red'));
   menuElement.classList.add('red');
 })
+//Scroll HEADER
+document.addEventListener('scroll',onScroll);
+
+function onScroll(event)
+{
+  const curPos = window.scrollY;
+document.querySelectorAll('main > div').forEach((el)=>{
+console.log(el.getAttribute('id'));
+//debugger;
+el.getAttribute('id');
+
+});
+}
+
+
+
+
 // PORTFOLIO
 image.addEventListener('click', (event) => {
   image.querySelectorAll('img').forEach(el => el.classList.remove('active_img'));
@@ -30,4 +50,79 @@ menuProfile.addEventListener('click', (event) => {
     shet++;
   }
 
+});
+
+//Slider
+let counter = 0;
+
+scrollLeft.addEventListener('click', (event) => {
+
+  ScrollClick();
+
+});
+
+scrollRight.addEventListener('click', (event) => {
+
+  ScrollClick();
+
+});
+
+scrollLeft1.addEventListener('click', (event) => {
+
+  ScrollClickTwo();
+
+});
+scrollRight1.addEventListener('click', (event) => {
+
+  ScrollClickTwo();
+
+});
+
+
+function ScrollClick() {
+  let interval = setInterval(function () {
+    document.querySelectorAll('.scroll').forEach((scroll) => {
+
+      if (scroll.style.left === '-1020px') {
+        clearInterval(interval);
+        return;
+      }
+      counter -= 10;
+      scroll.style.left = `${counter}px`;
+    })
+  }, 1);
+};
+
+function ScrollClickTwo() {
+  let interval = setInterval(function () {
+    document.querySelectorAll('.scroll').forEach((scroll) => {
+
+      if (scroll.style.left === '0px') {
+        clearInterval(interval);
+        return;
+      }
+      counter += 10;
+      scroll.style.left = `${counter}px`;
+    })
+  }, 1);
+
+}
+
+//phone off
+const HorizontalScreen = document.getElementById('iphone-horizontal');
+const VerticalScreen = document.getElementById('iphone_vertical');
+
+VerticalScreen.addEventListener('click', (event) => {
+
+  if(VerticalScreen.style.zIndex == '0'){ VerticalScreen.style.zIndex = 2;}
+  else VerticalScreen.style.zIndex = 0;
+  
+});
+
+
+HorizontalScreen.addEventListener('click', (event) => {
+  
+  if(HorizontalScreen.style.zIndex == '0' ){ HorizontalScreen.style.zIndex = 2;}
+  else HorizontalScreen.style.zIndex = 0;
+  
 });
